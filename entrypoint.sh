2c6498af2db3f6abd6ea52410f6c1b089c4fc516
senyoltw <senyoltw@users.noreply.github.com>
2021-06-10 11:03:41 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,7 @@ export GROUP_ID=$(id -g)
 
 if ! whoami >/dev/null 2>&1; then
     sed -i '/^toolbox/d' /etc/passwd
-    sed -i '/^root/d' /etc/group
     echo "toolbox:x:${USER_ID}:${GROUP_ID}::/home/toolbox:/bin/bash" >> /etc/passwd
-    echo "root:x:0:0,${USER_ID}:toolbox" >> /etc/group
 fi
 
 exec "$@"
