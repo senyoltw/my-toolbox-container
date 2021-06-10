38b6194329a31a9048258ce38679b10528689152
senyoltw <senyoltw@users.noreply.github.com>
2021-06-10 10:41:35 +0900

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
+if ! whoami >/dev/null 2>&1; then
+    sed -i '/^toolbox/d' /etc/passwd
+    sed -i '/^root/d' /etc/group
+          toolbox:x:1001:1001::/home/toolbox:/bin/bash
+    echo "toolbox:x:${USER_ID}:${GROUP_ID}::/home/toolbox:/bin/bash" >> /etc/passwd
+    echo "root:x:0:0,${USER_ID}:toolbox" >> /etc/group
+fi
+
+exec "$@"
