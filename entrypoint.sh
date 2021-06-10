fcedb904d0240850af31963ca84827ee100f9be2
senyoltw <senyoltw@users.noreply.github.com>
2021-06-10 12:35:07 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,6 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
 if ! grep -Fq "${USER_ID}" /etc/passwd; then
-    # current user is an arbitrary
-    # user (its uid is not in the
-    # container /etc/passwd). Let's fix that
     cat ${HOME}/passwd.template | \
     sed "s/\${USER_ID}/${USER_ID}/g" | \
     sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
