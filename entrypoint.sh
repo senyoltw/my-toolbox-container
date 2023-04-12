a9538a4a206fc627e160e7fdcf1da52a098498e4
senyoltw <senyoltw@users.noreply.github.com>
2023-04-12 15:35:47 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,11 @@ export GROUP_ID=$(id -g)
 if ! grep -Fq "${USER_ID}" /etc/passwd; then
     cat ${HOME}/passwd.template | \
     sed "s/\${USER_ID}/${USER_ID}/g" | \
-    sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
-    sed "s/\${HOME}/\/home\/jboss/g" > /etc/passwd
+    sed "s/\${GROUP_ID}/${GROUP_ID}/g" > /etc/passwd
 
     cat ${HOME}/group.template | \
     sed "s/\${USER_ID}/${USER_ID}/g" | \
-    sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
-    sed "s/\${HOME}/\/home\/jboss/g" > /etc/group
+    sed "s/\${GROUP_ID}/${GROUP_ID}/g" > /etc/group
 fi
 
 exec "$@"
